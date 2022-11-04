3e62e96fdf6b4a1a2a41d74d289acb71798a0c0d
GreekIgor <greekigor87@gmail.com>
2022-11-04 21:43:47 +0600

    new

--- app/src/main/java/com/greekasoft/weatherapp/MainActivity.kt
+++ app/src/main/java/com/greekasoft/weatherapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.greekasoft.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        main_city_tv.text = "Moskow"
+        main_whetarname.text = "Rain drop"
+        MainTempTxt.text = "25°"
+        maindatetv.text = "24 yuli 2022"
+        main_weather_image.setImageResource(R.mipmap.raincloud4x)
+
     }
 }
